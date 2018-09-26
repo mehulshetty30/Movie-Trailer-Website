89446dce2577b4dbfb416107ccdf42a5d124034d
mehulshetty30 <36084605+mehulshetty30@users.noreply.github.com>
2018-09-26 18:45:03 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,38 @@
+# ud036_StarterCode
+Source code for a Movie Trailer website.
+
+# Table of Contents:
+1. About
+2. How to run the code
+3. System Prerequisites
+4. License
+
+## About:
+
+This is a program called 'Mini IMDb'. Running this program will open your web webbrowser
+and take you to this amazing website 'Mini IMDb' where you can watch the trailers for
+all of your favourite movies.
+
+In order for this code to run, all you need to know is some python with a pinch of HTML and CSS.
+
+The two '.py' files contain:
+a. media.py - This file contains the class 'Movie' which can store information about the
+   movie's title, summary, poster image URL and link to its trailer.
+b. entertainment_center.py - This file contain multiple instances of the class 'Movie'.
+
+Keep in mind that the code was written in python (version 2.7.15).
+
+## How to run the code:
+
+1. Download the ZIP file (by clicking the 'Clone or Duplicate' button above).
+2. Open the 'Terminal' app. Type `python entertainment_center.py` in the terminal window.
+If the script is in a different directory from which you are trying to run it, you will need to type the full path to the script’s file in the terminal window, for example, `python /Users/myself/Downloads/Movie-Trailer-Website-master/entertainment_center.py` .
+
+## System Prerequisites:
+
+You need to install Python version 2.7 or above on your device to run this program.
+You can learn more about how to download Python in the link below:
+https://www.python.org/downloads/
+
+## License:
+   The content of this repository is licensed under a GNU GENERAL PUBLIC LICENSE (https://choosealicense.com/licenses/gpl-3.0/#).
---
+++ entertainment_center.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+import fresh_tomatoes
+import media
+
+hereditary = media.Movie("Hereditary",
+                         "After the family matriarch passes away, a \
+                         grieving family is haunted by tragic and \
+                         disturbing occurrences, and begin to unravel \
+                         dark secrets.",
+                         "https://upload.wikimedia.org/wikipedia/en/d/d9/Hereditary.png",  # NOQA
+                         "https://www.youtube.com/watch?v=V6wWKNij_1M")
+
+avengers_infinity_war = media.Movie("Avengers: Infinity War",
+                                    "The Avengers and their allies must\
+                                     be willing to sacrifice all in an \
+                                     attempt to defeat the powerful Thanos \
+                                     before his blitz of devastation and \
+                                     ruin puts an end to the universe.",
+                                    "https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg",  # NOQA
+                                    "https://www.youtube.com/watch?v=QwievZ1Tx-8")  # NOQA
+
+black_panther = media.Movie("Black Panther",
+                            "T'Challa, heir to the hidden but advanced \
+                            kingdom of Wakanda, must step forward to lead \
+                            his people into a new future and must confront \
+                            a challenger from his country's past.",
+                            "https://upload.wikimedia.org/wikipedia/en/0/0c/Black_Panther_film_poster.jpg",  # NOQA
+                            "https://www.youtube.com/watch?v=xjDjIWPwcPU")
+
+the_greatest_showman = media.Movie("The Greatest Showman",
+                                   "Celebrates the birth of show business,\
+                                    and tells of a visionary who rose from \
+                                    nothing to create a spectacle that\
+                                    became a worldwide sensation.",
+                                   "https://upload.wikimedia.org/wikipedia/en/1/10/The_Greatest_Showman_poster.png",  # NOQA
+                                   "https://www.youtube.com/watch?v=AXCTMGYUg9A")  # NOQA
+
+searching = media.Movie("Searching",
+                        "After his 16-year-old daughter goes missing, a \
+                        desperate father breaks into her laptop to look for \
+                        clues to find her.",
+                        "https://upload.wikimedia.org/wikipedia/en/b/b8/Searching.png",  # NOQA
+                        "https://www.youtube.com/watch?v=3Ro9ebQxEOY")
+
+bohemian_rhapsody = media.Movie("Bohemian Rhapsody",
+                                "A chronicle of the years leading up to\
+                                 Queen's legendary appearance at the Live \
+                                 Aid (1985) concert.",
+                                "https://upload.wikimedia.org/wikipedia/en/2/2e/Bohemian_Rhapsody_poster.png",  # NOQA
+                                "https://www.youtube.com/watch?v=mP0VHJYFOAU")
+
+it = media.Movie("It",
+                 "In the summer of 1989, a group of bullied kids band \
+                 together to destroy a shapeshifting monster, which \
+                 disguises itself as a clown and preys on the children \
+                 of Derry, their small Maine town.",
+                 "https://upload.wikimedia.org/wikipedia/en/5/5a/It_%282017%29_poster.jpg",  # NOQA
+                 "https://www.youtube.com/watch?v=FnCdOQsX5kc")
+
+the_shawshank_redemption = media.Movie("The Shawshank Redemption",
+                                       "Two imprisoned men bond over a \
+                                       number of years, finding solace and\
+                                        eventual redemption through acts of\
+                                         common decency.",
+                                       "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",  # NOQA
+                                       "https://www.youtube.com/watch?v=6hB3S9bIaco")  # NOQA
+
+mission_impossible_fallout = media.Movie("Mission: Impossible - Fallout",
+                                         "Ethan Hunt and his IMF team, along\
+                                          with some familiar allies, race \
+                                          against time after a mission gone\
+                                           wrong.",
+                                         "https://upload.wikimedia.org/wikipedia/en/f/ff/MI_–_Fallout.jpg",  # NOQA
+                                         "https://www.youtube.com/watch?v=wb49-oV0F78")  # NOQA
+
+oceans_eleven = media.Movie("Ocean's Eleven",
+                            "Danny Ocean and his eleven accomplices plan to\
+                             rob three Las Vegas casinos simultaneously.",
+                            "https://upload.wikimedia.org/wikipedia/en/thumb/6/68/Ocean%27s_Eleven_2001_Poster.jpg/220px-Ocean%27s_Eleven_2001_Poster.jpg",  # NOQA
+                            "https://www.youtube.com/watch?v=imm6OR605UI")
+
+the_dark_knight = media.Movie("The Dark Knight",
+                              "When the menace known as the Joker emerges \
+                              from his mysterious past, he wreaks havoc and\
+                               chaos on the people of Gotham. The Dark \
+                               Knight must accept one of the greatest \
+                               psychological and physical tests of his \
+                               ability to fight injustice.",
+                              "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",  # NOQA
+                              "https://www.youtube.com/watch?v=EXeTwQWrcwY")
+
+spirited_away = media.Movie("Spirited Away",
+                            "During her family's move to the suburbs, a \
+                            sullen 10-year-old girl wanders into a world \
+                            ruled by gods, witches, and spirits, \
+                            and where humans are changed into beasts.",
+                            "https://upload.wikimedia.org/wikipedia/en/thumb/d/db/Spirited_Away_Japanese_poster.png/220px-Spirited_Away_Japanese_poster.png",  # NOQA
+                            "https://www.youtube.com/watch?v=ByXuk9QqQkk")
+
+inception = media.Movie("Inception",
+                        "A thief who steals corporate secrets through the \
+                        use of dream-sharing technology is given the \
+                        inverse task of planting an idea into the mind\
+                          of a CEO.",
+                        "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",  # NOQA
+                        "https://www.youtube.com/watch?v=YoHD9XEInc0")
+
+life_is_beautiful = media.Movie("Life Is Beautiful",
+                                "When an open-minded Jewish librarian and \
+                                his son become victims of the Holocaust, he\
+                                 uses a perfect mixture of will, humor, and \
+                                 imagination to protect his son from the \
+                                 dangers around their camp.",
+                                "https://upload.wikimedia.org/wikipedia/en/thumb/7/7c/Vitaebella.jpg/220px-Vitaebella.jpg",  # NOQA
+                                "https://www.youtube.com/watch?v=pAYEQP8gx3w")
+
+interstellar = media.Movie("Interstellar",
+                           "A team of explorers travel through a wormhole in\
+                            space in an attempt to ensure humanity's \
+                            survival.",
+                           "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",  # NOQA
+                           "https://www.youtube.com/watch?v=zSWdZVtXT7E")
+
+the_lion_king = media.Movie("The Lion King",
+                            "A Lion cub crown prince is tricked by a \
+                            treacherous uncle into thinking he caused his \
+                            father's death and flees into exile in despair,\
+                             only to learn in adulthood his identity and \
+                             his responsibilities.",
+                            "https://upload.wikimedia.org/wikipedia/en/thumb/3/3d/The_Lion_King_poster.jpg/220px-The_Lion_King_poster.jpg",  # NOQA
+                            "https://www.youtube.com/watch?v=_ujGv5dhGfk")
+
+gravity = media.Movie("Gravity",
+                      "Two astronauts work together to survive after an \
+                      accident which leaves them stranded in space.",
+                      "https://upload.wikimedia.org/wikipedia/en/f/f6/Gravity_Poster.jpg",  # NOQA
+                      "https://www.youtube.com/watch?v=OiTiKOy59o4")
+
+wall_e = media.Movie("WALL.E",
+                     "In the distant future, a small waste-collecting robot\
+                      inadvertently embarks on a space journey that will \
+                      ultimately decide the fate of mankind.",
+                     "https://upload.wikimedia.org/wikipedia/en/thumb/c/c2/WALL-Eposter.jpg/220px-WALL-Eposter.jpg",  # NOQA
+                     "https://www.youtube.com/watch?v=alIq_wG9FNk")
+
+to_kill_a_mockingbird = media.Movie("To Kill a Mockingbird",
+                                    "Atticus Finch, a lawyer in the \
+                                    Depression-era South, defends a black\
+                                     man against an undeserved rape charge,\
+                                      and his children against prejudice.",
+                                    "https://upload.wikimedia.org/wikipedia/en/thumb/8/8e/To_Kill_a_Mockingbird_poster.jpg/220px-To_Kill_a_Mockingbird_poster.jpg",  # NOQA
+                                    "https://www.youtube.com/watch?v=KR7loA_oziY")  # NOQA
+
+monty_python_and_the_holy_grail = media.Movie("Monty Python and the Holy \
+                                               Grail",
+                                              "King Arthur and his Knights \
+                                              of the Round Table embark on a\
+                                               surreal, low-budget search \
+                                               for the Holy Grail, \
+                                               encountering many, very silly\
+                                                obstacles.",
+                                              "https://upload.wikimedia.org/wikipedia/en/thumb/0/08/Monty-Python-1975-poster.png/220px-Monty-Python-1975-poster.png",  # NOQA
+                                              "https://www.youtube.com/watch?v=urRkGvhXc8w")  # NOQA
+
+room = media.Movie("Room",
+                   "Held captive for years in an enclosed space, a woman and\
+                    her young son finally gain their freedom, allowing the \
+                    boy to experience the outside world for the first time.",
+                   "https://upload.wikimedia.org/wikipedia/en/c/c2/Room_%282015_film%29.png",  # NOQA
+                   "https://www.youtube.com/watch?v=E_Ci-pAL4eE")
+
+movies = [hereditary, avengers_infinity_war, black_panther,
+          the_greatest_showman, searching, bohemian_rhapsody, it,
+          the_shawshank_redemption, mission_impossible_fallout,
+          oceans_eleven, the_dark_knight, spirited_away, inception,
+          life_is_beautiful, interstellar, the_lion_king, gravity, wall_e,
+          to_kill_a_mockingbird, monty_python_and_the_holy_grail, room]
+
+fresh_tomatoes.open_movies_page(movies)
